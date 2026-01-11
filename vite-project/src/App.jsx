576d3d70b541cdfe0fda9612ae643b34f77c7907
guts-718 <rahul.20223195@mnnit.ac.in>
2026-01-11 13:04:41 +0530

    18. window-resize-hook

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -17,13 +17,14 @@ import FeatureFlagGlobalState from './components/feature_flag/context';
 import FeatureFlags from './components/feature_flag'
 import UseFetchTest from './components/use-fetch/test';
 import UseOnclickOutsideTest from './components/use-outside-click/test';
+import UseWindowResizeTest from './components/use-window-resize/test';
 function App() {
   
   
   return (
     
     <div className='App'>
-      <UseOnclickOutsideTest/>
+      <UseWindowResizeTest/>
     </div>
 
   )
